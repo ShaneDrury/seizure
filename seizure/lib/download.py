cc1aac7a9ca747cad0b3d0853a548a8ce2499ba2
Shane Drury <shane.r.drury@gmail.com>
2015-01-24 13:57:15 +0000

    Add download_to test

--- seizure/lib/download.py
+++ seizure/lib/download.py
@@ -1,14 +1,22 @@
+import requests
+
 from seizure.lib.paths import rename_extension, files_from_vod, generate_filename
 from seizure.lib.video import join_videos, convert_video
 
 
+def requests_download_file(response, to):
+    with open(to, 'wb') as f:
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            f.write(block)
+
+
 def download(url, to=None):
-    pass
-
-
-def download_multiple_videos(all_videos):
-    for vod in all_videos:
-        download_vod(vod)
+    to = to or url.split('/')[-1]
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    requests_download_file(response, to)
 
 
 def download_vod(vod):
@@ -17,6 +25,10 @@ def download_vod(vod):
     paths = [generate_filename(title, start_time, extension, n) for n in range(len(video_urls))]
     for vid, path in zip(video_urls, paths):
         download(vid, to=path)
+    return paths
+
+
+def convert_videos(paths):
     for saved in paths:
         convert_video(saved, to='mp4')
     converted_paths = [rename_extension(p, 'mp4') for p in paths]
--- seizure/tests/test_download.py
+++ seizure/tests/test_download.py
@@ -1,9 +1,28 @@
 import unittest
+from unittest.mock import patch, mock_open, MagicMock
+
+from requests import HTTPError
+
+from seizure.lib.download import download
+
+m = mock_open()
 
 
 class TestDownloads(unittest.TestCase):
-    def test_something(self):
+    def test_file_exists(self):
         self.assertEqual(True, False)
+
+    def test_url_doesnt_exist(self):
+        self.assertRaises(HTTPError, download, 'http://testfoo.com/files/test.flv')
+
+    @patch('seizure.lib.download.requests.get', autospec=True)
+    @patch('seizure.lib.download.open', new=m, create=True)
+    def test_download_to(self, mock_get):
+        url = 'http://testfoo.com/files/test.flv'
+        download(url)
+        m.assert_called_with('test.flv', 'wb')
+        download(url, to='bar.flv')
+        m.assert_called_with('bar.flv', 'wb')
 
 
 if __name__ == '__main__':
