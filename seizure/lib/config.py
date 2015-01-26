253e80cee56ecf5c05ced8b6af16eeede3071004
Shane Drury <shane.r.drury@gmail.com>
2015-01-26 22:38:41 +0000

    Set default values

--- seizure/lib/config.py
+++ seizure/lib/config.py
@@ -1,11 +1,20 @@
 import configparser
+import os
 
 
 class Config(object):
+    default_values = {
+        'FFmpeg Binary': 'ffmpeg',
+        'Download Folder': '.',
+        'Finished': ['', ],
+        }
+
     def __init__(self, filename):
         self.filename = filename
         self.config = configparser.ConfigParser()
         self.config.read(filename)
+        if not os.path.exists(self.filename):
+            self.write_initial()
 
     def started(self, filename):
         """Has the filename been started?"""
@@ -16,7 +25,6 @@ class Config(object):
         return filename in self.config['FINISHED']
 
     def write_initial(self):
-        self.config['STARTED'] = ['', ]
-        self.config['FINISHED'] = ['', ]
+        self.config['DEFAULT'] = self.default_values
         with open(self.filename, 'w') as f:
             self.config.write(f)
