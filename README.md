3bed330082199621ef66f51a058c3b80714dcf49
Shane Drury <shane.r.drury@gmail.com>
2015-01-28 23:09:03 +0000

    Add License, setup.cfg and README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+Seizure: Download Twitch VODs
+=============================
 [![Build Status](https://travis-ci.org/ShaneDrury/seizure.svg?branch=master)](https://travis-ci.org/ShaneDrury/seizure)
+
+Seizure is a library that facilitates downloading Twitch VODs. 
+
+.. code-block:: bash
+
+    $ seizure <video_code> --config config.ini
+    
+Installation
+------------
+To install Seizure, simply:
+
+.. code-block:: bash
+
+    $ mkvirtualenv seizure
+    $(seizure) python setup.py install
+
+Usage
+-----
+.. code-block:: bash
+
+    $ workon seizure
+    $(seizure) seizure 608495073
--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup
 
 setup(
     name='seizure',
-    version='',
+    version='1.0',
     packages=['seizure', 'seizure.lib'],
-    url='',
     license='',
     author='Shane Drury',
     author_email='shane.r.drury@gmail.com',
     description='Download VODs from Twitch',
     entry_points={'console_scripts': ['seizure = seizure.scripts:main'], },
+    keywords=['twitch', 'vod', 'download'],
+    url='https://github.com/ShaneDrury/seizure',
+    download_url='https://github.com/ShaneDrury/seizure/tarball/1.0',
     install_requires=['requests >= 2.5, < 2.6',
                       'progressbar2 >= 2.7 < 2.8'],
     )
