c4c9e17caa31d03c7f1a2db4b8492beb00449e74
Shane Drury <shane.r.drury@gmail.com>
2015-01-29 09:07:09 +0000

    Add folder to options

--- seizure/scripts.py
+++ seizure/scripts.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 
 from seizure.lib.config import Config
 from seizure.lib.conversion import Converter
@@ -17,14 +18,17 @@ def main():
     parser.add_argument('-quality', metavar='quality', type=str,
                         help='Quality of VOD: '
                              'One of 240p, 360p, 480p '
-                             'and live')
+                             'and live.')
+    parser.add_argument('-folder', metavar='folder', type=str,
+                        help='Folder to download the VOD to.')
     args = parser.parse_args()
     config = Config(args.config)
     quality = args.quality or config.quality
+    folder = args.folder or config.download_folder
     video = Video(args.code)
     downloader = Downloader(video, config)
     filenames = downloader.download(quality=quality,
-                                    folder=config.download_folder)
+                                    folder=folder)
     converter = Converter(config.ffmpeg_binary)
     converted = converter.convert(filenames)
     logging.info("Converted {}".format(converted))
